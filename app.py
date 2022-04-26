a00f636a49fe84985b3f489f00c76cdf54e0341c
Jorge Toledo <89712557+toledojorge@users.noreply.github.com>
2022-04-26 14:50:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/',methods=['GET'])
+def root():
+    return jsonify("ok /")
+
+@app.route('/api/health',methods=['GET'])
+def health():
+    return jsonify("ok /api/health")
+
 @app.route('/hello',methods=['GET'])
 def hello():
-    return jsonify({'mensaje' : 'Hello PPP'})
+    return jsonify({'mensaje' : 'Hello world'})
+
+@app.route('/headers',methods=['GET'])
+def headers():
+    return dict(request.headers)
 
 if __name__ == '__main__':
     app.run()
